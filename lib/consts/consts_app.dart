2167e61cad4265ee0007cf456ab308605ee3b6a1
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2020-11-03 13:51:29 -0300

    Added packeages, fonts and images. Started the home page.

---
+++ lib/consts/consts_app.dart
@@ -0,0 +1,4 @@
+class ConstsApp {
+  static const whitePokeball = 'images/pokeball.png';
+  static const BlackPokeball = 'images/pokeball_dark.png';
+}
--- lib/pages/home_page/widgets/home_page.dart
+++ lib/pages/home_page/widgets/home_page.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex_mobx/consts/consts_app.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Stack(
+          overflow: Overflow.visible,
+          children: [
+            Positioned(
+              top: -(240 / 3),
+              right: -(240 / 2.7),
+              child: Opacity(
+                child: Image.asset(
+                  ConstsApp.BlackPokeball,
+                  height: 240,
+                  width: 240,
+                ),
+                opacity: 0.1,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
